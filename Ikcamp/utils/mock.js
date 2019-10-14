70868cd3acaf2bf1b35403e95561bd2dd49208ed
x101x <1642372486@qq.com>
2019-10-14 17:42:06 +0800

    IKcamp

---
+++ Ikcamp/utils/mock.js
@@ -0,0 +1,31 @@
+const list = {
+  status: 200,
+  data: [{
+    data: "2019-08-15 ",
+    articles: [{
+        contentId: 1175110,
+        cover: "https://n1image.hjfile.cn/mh/2017/06/23/a5bf85e439696829a937accfd99f0eb7.jpg",
+        pubdate: "2017-06-25 00:45:00",
+        title: "Quora精选：什么样的人注定无法成功？"
+      },
+      {
+        contentId: 1142849,
+        cover: "https://n1image.hjfile.cn/mh/2017/06/22/faf544ccf3e4f9e8da4ee9d5d8fcc635.jpg",
+        pubdate: "2017-06-25 00:30:00",
+        title: "表达雌性动物和雄性动物的不同说法"
+      },
+      {
+        contentId: 1161915,
+        cover: "http://n1image.hjfile.cn/mh/2017/05/25/7778bd5547fa216db57cd87a78bd790d.jpg",
+        pubdate: "2017-06-25 00:15:00",
+        title: "2017年5月大学英语四级口语考试——如何用PPT展现一堂课"
+      },
+      {
+        contentId: 1175101,
+        cover: "https://n1image.hjfile.cn/mh/2017/06/23/045ed76502b0f1ccdaccdae827c17c2c.jpg",
+        pubdate: "2017-06-25 00:00:00",
+        title: "双语美文：总在不经意间，发觉自己渺小"
+      }
+    ]
+  }]
+}
